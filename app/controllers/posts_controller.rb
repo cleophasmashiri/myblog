dc32f2555150e7b6cafc0c05afa1abfaaac663ec
cleophas mashiri <cleophasmashiri@gmail.com>
2015-01-16 03:28:24 +0200

    list of post on home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,22 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all.order('created_at DESC')
     end
     def new
     end
     def create
-      @post = Post.new(Post_params)
+      @post = Post.new(post_params)
        @post.save
        redirect_to @post
     end
 
+    def show
+        @post = Post.find(params[:id])
+    end
+
+
     private
         def post_params
-            params.requre(:post),.permit(:title, :body):
+            params.require(:post).permit(:title, :body)
         end
 end
