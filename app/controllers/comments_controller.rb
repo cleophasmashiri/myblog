07b1a0ec56d2718bbd9943697baaf4bb85623278
cleophas mashiri <cleophasmashiri@gmail.com>
2015-01-17 16:16:11 +0200

    add authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
     def create
       @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(params[:comment].permit(:name, :body))
-      redirect_to post_path(@post)
+      if @post.comments.create(params[:comment].permit(:name, :body))
+          redirect_to post_path(@post)
+      else
+          #render post_path( @post)
+      end
     end
 
     def destroy
